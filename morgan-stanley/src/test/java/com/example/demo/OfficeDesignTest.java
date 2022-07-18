b79713ce35a29aeaf8c67ab7e84fdc9182b2dbd8
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-07-18 10:50:11 +0700

    update Office Design

--- morgan-stanley/src/test/java/com/example/demo/OfficeDesignTest.java
+++ morgan-stanley/src/test/java/com/example/demo/OfficeDesignTest.java
@@ -7,7 +7,16 @@ import java.util.List;
 public class OfficeDesignTest {
 
     public static int getMaxColors(List<Integer> prices, int money) {
+        // money = 7
         int sizePrices = prices.size();
+        // urutkan dulu prices nya, [1,1,1,2,3,5]
+        // lalu jumlah kan isi dalam prices 1 per satu mulai dari index awal, simpa ke variable totalPrice
+        // index1 + index2 = totalPriceTemp
+        // 1 + 1 = 2, lalu cek kondisi apakah <= money, jika iya maka jumlahkan lagi
+        // 2 + 1 = 3
+        // 3 + 2 = 5
+        // 5 + 3 (index4) = False karena 8 > 7
+        // lalu hitung jumlah index milik totalPrice
         return 0;
     }
 
