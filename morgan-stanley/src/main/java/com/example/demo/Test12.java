0ff01c8271b56ec09c8edb3c5a17139c07a59a81
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-07-18 11:00:51 +0700

    Test12

---
+++ morgan-stanley/src/main/java/com/example/demo/Test12.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+class Employee {
+    // employee attributes
+}
+
+class Accountant extends Employee {
+    // accountant attributes
+}
+
+public class Test12 {
+    public static void main(String[] args) {
+//        List<Employee> employees = new ArrayList<Accountant>();
+
+        // employees variable can hold object of Accountant class
+        // employees variable can hold objects of Accountant or Employee class
+
+    }
+}
+
